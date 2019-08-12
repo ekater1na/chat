316f729c31e56b7cf99cec18073292d74f4d5f3c
ekater1na <ekaterina_l@tut.by>
2019-08-13 00:37:44 +0300

    feat: add listener

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let app = require('http').createServer;
+let io = module.exports.io = require('socket.io')(app);
+
+const PORT = process.env.PORT || 3231;
+const SocketManager = require('./SocketManager');
+io.connection('connection', SocketManager);
+app.listen(PORT, ()=>{
+    console.log("Connected to port: " + PORT)
+}
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
