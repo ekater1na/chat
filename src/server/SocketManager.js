b985720bf8cd17502c8717d0f6266bc7acd5ef3b
ekater1na <ekaterina_l@tut.by>
2019-08-13 01:18:39 +0300

    feat: dependencies and package.json setup

--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 const io = require('./index.js').io;
+
 module.exports = function(socket){
     console.log('Socket ID:' + socket.id)
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-let app = require('http').createServer;
+let app = require('http').createServer();
 let io = module.exports.io = require('socket.io')(app);
 
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
-io.connection('connection', SocketManager);
+
+io.on('connection', SocketManager);
+
 app.listen(PORT, ()=>{
     console.log("Connected to port: " + PORT)
-}
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+})
