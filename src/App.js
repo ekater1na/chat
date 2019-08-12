53b80256fd67b387b51119af8e7807756e5e340a
ekater1na <ekaterina_l@tut.by>
2019-08-13 02:29:16 +0300

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import Layout from './components/Layout';
+import './styles/index.css';
 
 class App extends Component {
     render() {
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,7 @@ class Layout extends Component {
         };
     }
     
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
         this.initSocket()
     }
 
