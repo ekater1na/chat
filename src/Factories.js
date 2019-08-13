68541c1f2c6b16d251e2adae6c1b635c72d25847
ekater1na <ekaterina_l@tut.by>
2019-08-13 15:55:32 +0300

    feat:  create user, chat, message

---
+++ src/Factories.js
@@ -0,0 +1,36 @@
+const uuidv4 = require('uuid/v4');
+
+// Create a user
+const createUser = ({name = ""} = {})=>(
+{
+    id: uuidvv4(),
+    name
+})
+
+// Create message
+const createMessage = ({message = "", sender = ""} = {})=>(
+    {
+        id:uuidv4(),
+        time:getTime(new Date(Date.now())),
+        message,
+        sender
+    }
+)
+
+
+// Create chat
+const createChat = ({messages = [], name = "Community", user = []} = {})=>(
+    {
+        id:uuidv4(),
+        name,
+        messages,
+        users,
+        typingUsers:[]
+
+    }
+)
+
+// Chenge time format
+const getTime = (date)=>{
+    return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
+}
